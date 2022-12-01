e4de2b37e8ee80d3e219a7f9e6fbefd06ab9adb5
Daniel  Souza <104663666+DannielSouza@users.noreply.github.com>
2022-11-30 22:27:58 -0300

    loader color change and pre responsive header

--- client/src/components/Header.js
+++ client/src/components/Header.js
@@ -1,22 +1,39 @@
-import React from 'react'
-import style from '../styles/Header.module.css'
-import {Link} from 'react-router-dom'
+import React from "react";
+import style from "../styles/Header.module.css";
+import { Link } from "react-router-dom";
 
 const Header = () => {
   return (
     <header className={style.header}>
       <div className={style.headerContainer}>
-        <h2><Link className={style.title} to="/">Copa 2022</Link></h2>
+        <h2>
+          <Link className={style.title} to="/">
+            Copa 2022
+          </Link>
+        </h2>
 
-        <nav className={style.menu}>
-          <li><Link className={style.menuItem} to={"/"}>Fase de grupos</Link></li>
-          <li><Link className={style.menuItemDisabled}>Oitavas</Link></li>
-          <li><Link className={style.menuItemDisabled}>Quartas</Link></li>
-          <li><Link className={style.menuItemDisabled}>Semifinal e final</Link></li>
+        <nav id={style.navId} class={style.nav}>
+          <button id={style.btnMobile}>
+            <span id={style.hamburger}></span>
+          </button>
+
+          <div>
+            <ul id={style.menuDesktop} class={style.menuDesktop}>
+              <li class={style.menuDesktopItem}>
+                <Link>Quartas</Link>
+              </li>
+              <li class={style.menuDesktopItemDisabled}>
+                <Link>Oitavas</Link>
+              </li>
+              <li class={style.menuDesktopItemDisabled}>
+                <Link>Semifinal e final</Link>
+              </li>
+            </ul>
+          </div>
         </nav>
       </div>
     </header>
-  )
-}
+  );
+};
 
-export default Header
+export default Header;
